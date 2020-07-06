716fbaec259eeaddbc87b8c72228415845fb448d
VandersalAheram <201224@ids.upchiapas.edu.mx>
2020-07-06 18:32:09 -0500

    Codigos

---
+++ CodigosGenericos/Codigo1/Principal.java
@@ -0,0 +1,12 @@
+public class Principal{
+    public static void main(String[] args) {
+        //Casteo de variables
+
+        int variableX = 50, variableY = 10;
+        float variableZ = 5.5f;
+        float resultado;
+
+        resultado = (float)variableX + variableZ;
+        System.out.println(resultado);
+    }
+}
